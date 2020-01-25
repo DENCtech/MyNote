9dea7ae9a36cf246f172d389fe8b6aa7626b7a0d
Chukwuemeka Nwocha Jnr <Chukwuemeka Nwocha Jnr>
2020-01-25 20:34:12 +0100

    Add project files.

---
+++ MyNote/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyNote.Startup))]
+namespace MyNote
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
